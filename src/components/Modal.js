0d190e6656af20a90e270ac59fea85cfcdd2c9a7
VIGNESH N <65857628+vignesh2718@users.noreply.github.com>
2022-08-05 12:49:21 +0530

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,12 +4,16 @@ function Modal(props) {
     //   function hideDetails(){
     //     setModal(false);
     //   }
+    const openInNewTab = url => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+      };
     return (
         <div className="modal">
             <button  className="btn" onClick={props.onClick}> ❌ </button>
             <img src={props.img} className="modal-img" />
             <h3>{props.title}</h3>
             <p>{props.desc} {props.year} {props.genere}</p>
+ <button className="btn1" onClick={() => openInNewTab('https://www.hotstar.com/in/movies/avengers-infinity-war/1660010677') }>Open</button>
               
             
         </div>
